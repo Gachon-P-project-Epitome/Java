cff2330b2f779d5f788fc98080088e73703a1380
hbs5186 <hbs5186@naver.com>
2024-09-22 19:30:56 +0900

    find_track

--- src/main/java/spotify/CreateToken.java
+++ src/main/java/spotify/CreateToken.java
@@ -4,22 +4,33 @@ import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
 import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import jakarta.annotation.PostConstruct;
 import org.apache.hc.core5.http.ParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Service
 public class CreateToken {
-    private static final String clientId = "345d71b717834eb7a4c0136d98112fe0";
-    private static final String clientSecret = "9b65b554fe7f4d47a6cddff37b9662f3";
-    private static SpotifyApi spotifyApi = new SpotifyApi.Builder()
-            .setClientId(clientId)
-            .setClientSecret(clientSecret)
-            .build();
+    @Value("${spotify.clientId}")
+    private String clientId;
+
+    @Value("${spotify.clientSecret}")
+    private String clientSecret;
+
+    private SpotifyApi spotifyApi;
+
+    @PostConstruct
+    public void init() {
+        spotifyApi = new SpotifyApi.Builder()
+                .setClientId(clientId)
+                .setClientSecret(clientSecret)
+                .build();
+    }
 
     // Access token을 반환하는 메서드
-    public static String accesstoken() {
+    public String accesstoken() {
         ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
         try {
             final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
@@ -36,5 +47,4 @@ public class CreateToken {
             return null;
         }
     }
-
 }
